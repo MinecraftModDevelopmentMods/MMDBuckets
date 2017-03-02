31fd49cab92cf8891cce2e31b5c4ae8eb6203f09
Daniel Hazelton <dshadowwolf@gmail.com>
2017-03-01 21:39:22 -0500

    Even more meat for the skeleton! We now have, somewhat, the different buckets as one item!

--- src/main/java/com/mcmoddev/buckets/init/Items.java
+++ src/main/java/com/mcmoddev/buckets/init/Items.java
@@ -1,7 +1,40 @@
 package com.mcmoddev.buckets.init;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import com.mcmoddev.buckets.items.ItemMMDBucket;
+import com.mcmoddev.buckets.util.Config;
+import com.mcmoddev.lib.material.MetalMaterial;
+import com.mcmoddev.lib.material.MetalMaterial.MaterialType;
+
+import net.minecraft.init.Blocks;
+
 public class Items extends com.mcmoddev.lib.init.Items {
+	
+	protected static List<ItemMMDBucket> buckets = new ArrayList<>();
+
 	public static void init() {
+		Collection<MetalMaterial> materials = Materials.getAllMaterials();
 		
+		for( MetalMaterial mat : materials ) {
+			if( Config.get(mat.getName()) && mat.getType() == MaterialType.METAL ) {
+				buckets.add(new ItemMMDBucket(Blocks.AIR, mat));
+			}
+		}
+	}
+	
+	public static int getCount() {
+		return buckets.size();
+	}
+	
+	public static List<ItemMMDBucket> getBuckets() {
+		return Collections.unmodifiableList(buckets);
+	}
+	
+	public static String getNameFromMeta( int meta ) {
+		return buckets.get(meta).getMaterial().getName();
 	}
 }
--- src/main/java/com/mcmoddev/buckets/items/ItemMMDBucket.java
+++ src/main/java/com/mcmoddev/buckets/items/ItemMMDBucket.java
@@ -4,17 +4,31 @@ import com.mcmoddev.lib.material.IMetalObject;
 import com.mcmoddev.lib.material.MetalMaterial;
 import com.mcmoddev.lib.registry.IOreDictionaryEntry;
 
+import java.util.List;
+
+import com.mcmoddev.buckets.init.Items;
+
 import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBucket;
+import net.minecraft.item.ItemStack;
+
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemMMDBucket extends ItemBucket implements IOreDictionaryEntry, IMetalObject {
 
 	private final MetalMaterial baseMaterial;
+	private static final int numBuckets = Items.getCount();
 	
 	public ItemMMDBucket(Block containedBlockIn, MetalMaterial base) {
 		super(containedBlockIn);
 		this.baseMaterial = base;
 		this.setMaxStackSize(1);
+		this.setHasSubtypes(true);
+		this.setCreativeTab(CreativeTabs.MISC);
+		this.setMaxDamage(0);
 	}
 
 	@Override
@@ -32,4 +46,16 @@ public class ItemMMDBucket extends ItemBucket implements IOreDictionaryEntry, IM
 		return "bucket"+this.baseMaterial.getCapitalizedName();
 	}
 
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+		return "item.bucket." + Items.getNameFromMeta(stack.getMetadata()) + ".name";
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+		subItems.add(new ItemStack(itemIn));
+		for( int i = 0; i < numBuckets; i++ ) {
+			subItems.add( new ItemStack(itemIn, 1, i));
+		}
+	}
 }
